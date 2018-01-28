3d962e341798f28e6967f5196aa8387a8d05d6dc
igorbr <Igor.Bronovytskyi@playtech.com>
2018-01-28 15:22:29 +0200

    refactoring

--- js/main.js
+++ js/main.js
@@ -1,16 +1,7 @@
 window.addEventListener('load', function() {
-    var click = document.querySelectorAll('dt');
-    var body = document.querySelectorAll('dd');
-
-    click[0].addEventListener('click', function() {
-        body[0].classList.toggle('active')
-    });
-
-    click[1].addEventListener('click', function() {
-        body[1].classList.toggle('active')
-    });
-
-    click[2].addEventListener('click', function() {
-        body[2].classList.toggle('active')
+    document.querySelector('.module--faq').addEventListener('click', function (event) {
+        if (event.target.tagName === 'DT') {
+            event.target.classList.toggle('active');
+        }
     });
 });
